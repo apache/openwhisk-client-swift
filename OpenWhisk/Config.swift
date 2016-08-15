88e960d4cb1dab28e4736052c1ab313a03c60633
Paul Castro <castrop@us.ibm.com>
2016-08-15 11:32:18 -0400

    Add launch arguments

--- OpenWhisk/Config.swift
+++ OpenWhisk/Config.swift
@@ -1,25 +1,28 @@
 /*
-* Copyright 2015-2016 IBM Corporation
-*
-* Licensed under the Apache License, Version 2.0 (the "License");
-* you may not use this file except in compliance with the License.
-* You may obtain a copy of the License at
-*
-* http://www.apache.org/licenses/LICENSE-2.0
-*
-* Unless required by applicable law or agreed to in writing, software
-* distributed under the License is distributed on an "AS IS" BASIS,
-* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-* See the License for the specific language governing permissions and
-* limitations under the License.
-*/
+ * Copyright 2015-2016 IBM Corporation
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 
 import Foundation
 
 /*
  Retrieves basic configuration information for the SDK specified in WhiskConfig.plist or environment variables
-*/
+ */
 public class Config {
+    
+    static let ApiKeyName = "OPENWHISK_TESTAPIKEY"
+    static let ApiSecretName = "OPENWHISK_TESTAPISECRET"
     
     public class func getHostAndPath(type type:String) -> String? {
         
@@ -85,14 +88,41 @@ public class Config {
     /*
      Can be used to read authentication credentials from env variables.  Useful for unit tests and maybe some build tasks
      but not much else?
-    */
+     */
     public class func getAuthToken() -> (apiKey: String?, apiSecret: String?)? {
         
-        let dict = NSProcessInfo.processInfo().environment
-        let key = dict["TESTAPIKEY"]
-        let secret = dict["TESTAPISECRET"]
         
-        return(key, secret)
+        let args = NSProcessInfo.processInfo().arguments
+        
+        var getKey = false
+        var getToken = false
+        var key: String? = nil
+        var token: String? = nil
+        
+        for arg in args {
+            if arg == "-\(Config.ApiKeyName)" {
+                getKey = true
+            } else if arg == "-\(Config.ApiSecretName)" {
+                getToken = true
+            } else if getKey == true {
+                key = arg
+                getKey = false
+            } else if getToken == true {
+                token = arg
+                getToken = false
+            }
+        }
+        print("Command line: Key: \(key), Token: \(token)")
+        
+        // complete read from env variables if not passed in from command line
+        if key == nil || token == nil {
+            let dict = NSProcessInfo.processInfo().environment
+            
+            key = dict[Config.ApiKeyName]
+            token = dict[Config.ApiSecretName]
+        }
+        
+        return(key, token)
     }
     
 }
