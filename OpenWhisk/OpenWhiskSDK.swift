7062e6848d516b70d76606c4693c5ab8ac78ad42
Matt Rutkowski <mrutkows@us.ibm.com>
2017-07-06 12:03:03 -0500

    Update .swift and .h files to ASF license headers.

--- OpenWhisk/OpenWhiskSDK.swift
+++ OpenWhisk/OpenWhiskSDK.swift
@@ -1,18 +1,19 @@
 /*
-* Copyright 2015-2016 IBM Corporation
-*
-* Licensed under the Apache License, Version 2.0 (the "License");
-* you may not use this file except in compliance with the License.
-* You may obtain a copy of the License at
-*
-* http://www.apache.org/licenses/LICENSE-2.0
-*
-* Unless required by applicable law or agreed to in writing, software
-* distributed under the License is distributed on an "AS IS" BASIS,
-* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-* See the License for the specific language governing permissions and
-* limitations under the License.
-*/
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 
 import Foundation
 
@@ -29,16 +30,16 @@ public struct WhiskCredentials {
         self.sessionToken = sessionToken
         self.jwtToken = jwtToken
     }
-    
+
     // whisk credentials
     public var accessKey: String?
     public var accessToken: String?
     public var sessionToken: String?
-    
+
     // optional app credentials
     public var appKey: String?
     public var appSecret: String?
-    
+
     // optional token for custom authentication flow
     public var jwtToken: String?
 }
@@ -60,39 +61,39 @@ enum WhiskType {
 
 /* Main class to hold the calls to invoke Actions and fire Triggers */
 public class Whisk {
-    
+
     // Secrets needed to call Whisk API
     let AccessKey: String? // Whisk key
     let AccessToken: String? // Whisk token
     let AppKey: String? // application Key (currently not used)
     let AppSecret: String? // application Secret (curently not used)
-    
+
     // api Host for Whisk backend
     public var whiskBaseURL: String?
-    
+
     // set to non-nil if using a custom session
     public var urlSession: URLSession?
-    
+
     public var verboseReplies: Bool = false
-    
+
     // Set these if you want to run unit tests and mock
     // calls to Whisk backend.
     public var useMock: Bool = false
     public var mockReply: [String: AnyObject]?
     public var mockError: WhiskError?
-    
-    
+
+
     // return base URL of backend including common path for all API calls
     public var baseURL: String? {
         set {
             if let url = newValue {
-                
+
                 let c = url.characters.last
-                
+
                 let separater =  c == "/" ? "" : "/"
-                
+
                 whiskBaseURL = url + separater + "api/v1/"
-                
+
             } else {
                 whiskBaseURL = nil
             }
@@ -101,7 +102,7 @@ public class Whisk {
             return whiskBaseURL
         }
     }
-    
+
     // Initialize with credentials, region currently not used
     public init(credentials: WhiskCredentials, region: String = "US-East-1") {
         // initialize
@@ -109,28 +110,28 @@ public class Whisk {
         AccessToken = credentials.accessToken
         AppKey = credentials.appKey
         AppSecret = credentials.appSecret
-        
+
     }
-    
-    
+
+
     /* Base function to fire Whisk Trigger identified by qualified name */
     public func fireTrigger(qualifiedName: String, parameters: AnyObject? = nil, callback: @escaping (Dictionary<String,AnyObject>?, WhiskError?)->Void) throws {
-        
+
         let pathParts = try Whisk.processQualifiedName(qualifiedName)
         try fireTrigger(name: pathParts.name, package: pathParts.package, namespace: pathParts.namespace, parameters: parameters, callback: callback)
     }
-    
+
     /* Base function to invoke Whisk Action identified by qualified name */
     public func invokeAction(qualifiedName: String, parameters: AnyObject?, hasResult: Bool = false, callback: @escaping (Dictionary<String,AnyObject>?, WhiskError?)->Void) throws {
-        
+
         let pathParts = try Whisk.processQualifiedName(qualifiedName)
         try invokeAction(name: pathParts.name, package: pathParts.package, namespace: pathParts.namespace, parameters: parameters, hasResult: hasResult, callback: callback)
     }
-    
-    
+
+
     /* Base function to fire Whisk Trigger identified by components */
     public func fireTrigger(name: String, package: String? = nil, namespace: String = "_", parameters: AnyObject? = nil, callback: @escaping (Dictionary<String,AnyObject>?, WhiskError?)->Void) throws {
-        
+
         if let accessKey = AccessKey, let accessToken = AccessToken {
             try httpRequestWhiskAPI(accessKey: accessKey, accessToken: accessToken, namespace: namespace, verb: "POST", type: .trigger, package: package, name:name, parameters: parameters, isSync: false, callback: { (jsonArray, error) in
                 if let error = error {
@@ -142,43 +143,43 @@ public class Whisk {
         } else {
             throw WhiskError.credentialError(description: "Access key and token not set")
         }
-        
-        
+
+
     }
-    
+
     /* Base function to invoke Whisk Action identified by components */
     public func invokeAction(name: String, package: String? = nil, namespace: String = "_", parameters: AnyObject?, hasResult:Bool = false, callback: @escaping (Dictionary<String,AnyObject>?, WhiskError?)-> Void) throws {
         if let accessKey = AccessKey, let accessToken = AccessToken {
-            
+
             try httpRequestWhiskAPI(accessKey: accessKey, accessToken: accessToken, namespace: namespace, verb: "POST", type: .action, package: package, name: name, parameters: parameters, isSync: hasResult, callback: {(jsonDict, error) in
                 if let error = error {
                     callback(nil, error)
                 } else {
                     callback(jsonDict, nil)
                 }
-                
+
             })
         } else {
             throw WhiskError.credentialError(description: "Access key and token not set")
         }
-        
+
     }
-    
+
     /* can redirect call here, e.g. if mocking */
     func httpRequestWhiskAPI(accessKey: String, accessToken: String, namespace: String, verb: String, type: WhiskType, package: String?, name: String, parameters: AnyObject?, isSync: Bool, callback: @escaping (Dictionary<String,AnyObject>?, WhiskError?) ->Void) throws {
-        
+
         if useMock {
             callback(mockReply, mockError)
-            
+
         } else {
             try whiskAPI(accessKey: accessKey, accessToken: accessToken, namespace: namespace, verb: verb, type: type, package: package, name: name, parameters: parameters, isSync: isSync, callback: callback)
         }
     }
-    
-    
+
+
     /* Network call */
     func whiskAPI(accessKey: String, accessToken: String, namespace: String, verb: String, type: WhiskType, package: String?, name: String, parameters: AnyObject?, isSync: Bool, callback:  @escaping (Dictionary<String,AnyObject>?,WhiskError?) ->Void) throws {
-        
+
         // set parameters
         var paramsIsDict = false
         if let parameters = parameters {
@@ -186,14 +187,14 @@ public class Whisk {
                 paramsIsDict = true
             }
         }
-        
+
         // set authorization string
         let loginString = NSString(format: "%@:%@", accessKey, accessToken)
         let loginData: Data = loginString.data(using: String.Encoding.utf8.rawValue)!
         let base64LoginString = loginData.base64EncodedString(options: NSData.Base64EncodingOptions(rawValue: 0))
-        
+
         let typeStr: String!
-        
+
         // set type
         switch type {
         case .action:
@@ -201,22 +202,22 @@ public class Whisk {
         case .trigger:
             typeStr = "triggers"
         }
-        
+
         // get base URL
         guard let actionURL = baseURL != nil ? baseURL : Config.getHostAndPath(type: typeStr) else {
             callback(nil, WhiskError.httpError(description: "Base URL not set, try using whisk.baseUrl setting", statusCode: 400))
             return
         }
-        
+
         // append namespace and trigger/action path
         var syncName = "namespaces/"
-        
+
         var namespaceStr = namespace
-        
+
         if namespace.characters.count == 0 {
             namespaceStr = "_"
         }
-        
+
         if let package = package {
             if package.characters.count == 0 {
                 syncName = syncName + namespaceStr+"/"+typeStr+"/"+name
@@ -226,40 +227,40 @@ public class Whisk {
         } else {
             syncName = syncName + namespaceStr+"/"+typeStr+"/"+name
         }
-        
+
         // if action has results, specify as blocking
         if isSync == true {
             syncName += "?blocking=true"
         }
-        
+
         // use this for verbose replies
         let restCall = actionURL+syncName
-        
+
         guard let encodedPath = syncName.addingPercentEncoding(withAllowedCharacters: CharacterSet.urlQueryAllowed) else {
             callback(nil, WhiskError.httpError(description: "URL Encode error \(syncName)", statusCode: 400))
             return
         }
-        
+
         syncName = encodedPath
-        
+
         // create request
         guard let url = URL(string:actionURL+syncName) else {
             // send back error on main queue
-            
+
             callback(nil, WhiskError.httpError(description: "Malformed url \(actionURL+syncName)", statusCode: 400))
-            
+
             return
-            
+
         }
-        
+
         var request = URLRequest(url: url)
         request.setValue("application/json; charset=utf-8", forHTTPHeaderField: "Content-Type")
         request.addValue("Basic \(base64LoginString)", forHTTPHeaderField: "Authorization")
         request.httpMethod = verb
-        
+
         // create JSON from parameters dictionary
         do {
-            
+
             if let parameters = parameters {
                 if paramsIsDict {
                     request.httpBody = try JSONSerialization.data(withJSONObject: parameters, options: JSONSerialization.WritingOptions())
@@ -273,12 +274,12 @@ public class Whisk {
                     }
                 }
             }
-            
+
         } catch {
             print("Error parsing JSON in Whisk request: \(error)")
         }
-        
-        
+
+
         // retrieve session as default or use developer specified session
         let sess: URLSession!
         if let _ = urlSession {
@@ -287,14 +288,14 @@ public class Whisk {
             let sessConfig = URLSessionConfiguration.default
             sess = URLSession(configuration: sessConfig)
         }
-        
+
         // perform network request
         let task = sess.dataTask(with: request) {
             data, response, error in
             let statusCode: Int!
-            
+
             if let error = error {
-                
+
                 if let httpResponse = response as? HTTPURLResponse {
                     statusCode = httpResponse.statusCode
                 } else {
@@ -304,28 +305,28 @@ public class Whisk {
                 DispatchQueue.main.async {
                     callback(nil, WhiskError.httpError(description: "\(error.localizedDescription)", statusCode: statusCode))
                 }
-                
+
                 return
-                
+
             } else {
-                
+
                 if let httpResponse = response as? HTTPURLResponse {
                     statusCode = httpResponse.statusCode
                     do {
                         // success
                         if statusCode < 300 {
-                            
+
                             switch verb {
                                 // is an action invocation
                             case "POST":
                                 var jsonDict = [String:AnyObject]()
-                                
+
                                 let respDict = try JSONSerialization.jsonObject(with: data!, options: JSONSerialization.ReadingOptions.mutableContainers) as! Dictionary<String, AnyObject>
                                 jsonDict = respDict
-                                
-                                
+
+
                                 if let whiskError = jsonDict["error"] as? String {
-                                    
+
                                     var errorCode = -1
                                     if let code = jsonDict["code"] as? Int {
                                         errorCode = code
@@ -334,14 +335,14 @@ public class Whisk {
                                     DispatchQueue.main.async {
                                         callback(nil, WhiskError.whiskProcessingError(description: "errorCode:\(errorCode), \(whiskError)", errorCode: errorCode))
                                     }
-                                    
+
                                 } else {
-                                    
+
                                     var whiskReply = [String:AnyObject]()
-                                    
+
                                     if self.verboseReplies == true {
                                         whiskReply = jsonDict
-                                        
+
                                         // add the rest call made to verbose replies for debugging
                                         switch type {
                                         case .action:
@@ -349,24 +350,24 @@ public class Whisk {
                                         case .trigger:
                                             whiskReply["triggerUrl"] = restCall as AnyObject?
                                         }
-                                        
+
                                     } else {
                                         let reply = jsonDict
                                         whiskReply["activationId"] = reply["activationId"]
-                                        
+
                                         if isSync == true {
                                             if let whiskResponse = reply["response"] as? [String:AnyObject] {
-                                                
+
                                                 if let actionResult = whiskResponse["result"] {
-                                                    
+
                                                     //if let payload = actionResult["payload"] {
-                                                    
+
                                                     let payload:AnyObject? = actionResult
                                                     if payload is String {
                                                         do {
-                                                            
+
                                                            let payloadObj =  try JSONSerialization.jsonObject(with: (payload as! String).data(using: String.Encoding.utf8)!, options: [])
-                                                           
+
                                                             whiskReply["result"] = payloadObj as AnyObject
                                                         } catch {
                                                             print("Error parsing payload into JSON, defaulting to string")
@@ -380,33 +381,33 @@ public class Whisk {
                                             }
                                         }
                                     }
-                                    
+
                                     // send back successful response on main queue
                                     DispatchQueue.main.async {
                                         callback(whiskReply, nil)
                                     }
                                 }
-                                
+
                                 // get info about actions/triggers
                                 // not used right now
                             case "GET":
                                 let jsonArray = try JSONSerialization.jsonObject(with: data!, options: JSONSerialization.ReadingOptions.mutableContainers) as! NSArray
                                 let jsonDict:Dictionary<String, AnyObject> = ["array":jsonArray]
-                                
+
                                 DispatchQueue.main.async {
                                     callback(jsonDict, nil)
                                 }
-                                
+
                             default:
                                 break
-                                
+
                             }
                         } else {
                             DispatchQueue.main.async {
                                 callback(nil, WhiskError.httpError(description: "Whisk returned HTTP error code", statusCode: statusCode))
                             }
                         }
-                        
+
                     } catch {
                         print("Error parsing JSON from Whisk response: \(error)")
                         DispatchQueue.main.async {
@@ -416,25 +417,25 @@ public class Whisk {
                 }
             }
         }
-        
+
         task.resume()
-        
-        
+
+
     }
-    
+
     /* Convert qualified name string into component parts of action or trigger call */
     class func processQualifiedName(_ qName: String) throws -> (namespace:String, package: String?, name: String) {
         var namespace = "_"
         var package: String? = nil
         var name = ""
         var doesSpecifyNamespace = false
-        
+
         if qName.characters.first == "/" {
             doesSpecifyNamespace = true
         }
-        
+
         let pathParts = qName.characters.split { $0 == "/" }.map(String.init)
-        
+
         if doesSpecifyNamespace == true {
             if pathParts.count == 2 {
                 namespace = pathParts[0]
@@ -456,10 +457,10 @@ public class Whisk {
                 throw WhiskError.qualifiedNameFormat(description: "Cannot parse \(qName)")
             }
         }
-        
+
         return (namespace, package, name)
     }
-    
+
 }
 
 
--- mobile/iOS/starterapp/OpenWhiskStarterApp/ResultSetTableController.swift
+++ mobile/iOS/starterapp/OpenWhiskStarterApp/ResultSetTableController.swift
@@ -1,50 +1,50 @@
 /*
-* Copyright 2015-2016 IBM Corporation
-*
-* Licensed under the Apache License, Version 2.0 (the "License");
-* you may not use this file except in compliance with the License.
-* You may obtain a copy of the License at
-*
-* http://www.apache.org/licenses/LICENSE-2.0
-*
-* Unless required by applicable law or agreed to in writing, software
-* distributed under the License is distributed on an "AS IS" BASIS,
-* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-* See the License for the specific language governing permissions and
-* limitations under the License.
-*/
-
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 
 import UIKit
 
 class ResultSetTableController: UITableViewController {
-    
+
     var resultSet: [String: AnyObject]!
     var names = [String]()
     var values = [String]()
     var isComplex = [Bool]()
-    
+
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        
+
+
         tableView.estimatedRowHeight = 44
         tableView.rowHeight = UITableViewAutomaticDimension
-        
+
         navigationItem.title = "Action Results"
-        
+
         reloadTable()
     }
-    
+
     func reloadTable() {
-        
+
         names = [String]()
         values = [String]()
-        
+
         if let resultSet = resultSet , resultSet.count > 0 {
             for (name, value) in resultSet {
                 names.append(name)
-                
+
                 if let value = (value as? String) {
                     values.append(value)
                     isComplex.append(false)
@@ -77,31 +77,31 @@ class ResultSetTableController: UITableViewController {
                 }
             }
         }
-        
-        
+
+
     }
-    
-    
+
+
     // MARK UITableViewDataSource
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return resultSet.count
     }
-    
+
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "resultCell") as! ResultSetCell
-        
+
         cell.nameLabel.text = names[(indexPath as NSIndexPath).row]
         cell.valueLabel.text = values[(indexPath as NSIndexPath).row]
-        
+
         if isComplex[(indexPath as NSIndexPath).row] == true {
             cell.accessoryType = UITableViewCellAccessoryType.disclosureIndicator
         }
-        
-        
+
+
         return cell
-        
+
     }
-    
-    
-    
+
+
+
 }
